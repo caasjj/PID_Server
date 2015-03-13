d1e66caede2b76d3c2c80aaad898ec484f642bf9
Walid Hosseini <caasjj@gmail.com>
2015-03-13 13:57:05 -0700

    send down commands - need to fix buffer size/messageLength bug

--- app.js
+++ app.js
@@ -1,139 +1,26 @@
-var serialport = require('serialport');
-var SerialPort = serialport.SerialPort;
-var through2 = require('through2');
-var fs = require('fs');
+var arduinoCom = require('./lib/arduino-com.js');
 
-var dump = fs.createWriteStream('dump');
+var count = 0;
 
-var header  = new Buffer([0x44, 0x33, 0x22, 0x11]);
-var trailer = new Buffer([0xBB, 0xCC, 0xDD, 0xEE]);
-
-
-function MSGParser(startDelimiter, tailDelimiter) {
-
-    var _sync = false;
-    var previous = new Buffer(0);
-    var msgLength;
-
-    return function(emitter, buffer) {
-
-        var startBuffer,tailBuffer, message;
-
-        buffer = Buffer.concat([previous, buffer]);
-
-        var index = 0;
-        while (!_sync && index < (buffer.length - startDelimiter.length + 3) ) {
-            startBuffer = buffer.slice(index, index + startDelimiter.length);
-            if( startBuffer.equals(startDelimiter) ) {
-                _sync = true;
-                msgLength = buffer.readUInt16BE(index + startDelimiter.length);
-            } else {
-                index++;
-            }
-        }
-
-        while (_sync && index < buffer.length - startDelimiter.length - tailDelimiter.length - msgLength - 1) {
-
-            startBuffer = buffer.slice( index,
-                index+startDelimiter.length);
-
-            msgLength = buffer.readUInt16BE(index + startDelimiter.length);
-            //console.log('Length: ', msgLength);
-            tailBuffer = buffer.slice( index+startDelimiter.length+msgLength,
-                index+startDelimiter.length+msgLength+tailDelimiter.length);
-
-            if (startBuffer.equals(startDelimiter) && tailBuffer.equals(tailDelimiter)) {
-                index += startDelimiter.length;
-                message = buffer.slice(index, index+msgLength+2 );
-                emitter.emit( 'data', message );
-                //console.log(message);
-                index += msgLength + tailDelimiter.length;
-            }
-            else {
-                index += startDelimiter.length;
-                _sync = false;
-            }
-        }
-
-        previous = buffer.slice(index, buffer.length);
-
-    }
+var command = {
+    type: 2,
+    enable: false
 }
 
 
-var sp = new SerialPort("/dev/tty.usbmodemfa1231", {
-    baudrate: 115200,
-    parser: MSGParser(header, trailer)
-}, false); // this is the openImmediately flag [default is true]
+arduinoCom({baudRate: 115200}, function(err, stream, writer) {
+    if (err) return err;
 
-function parseDataMessage(chunk, length) {
+    stream.pipe(process.stdout);
 
-    var startWord = 0;
-    var output =  {
-        timeStamp: chunk.readUInt32LE(startWord, true),
-        adcInput : chunk.readFloatLE(startWord+4, true),
-        pidOutput: chunk.readFloatLE(startWord+8, true),
-        setPoint : chunk.readFloatLE(startWord+12, true),
-        dispKp: chunk.readFloatLE(startWord+16, true),
-        dispKi: chunk.readFloatLE(startWord+20, true),
-        dispKd: chunk.readFloatLE(startWord+24, true),
-        kp: chunk.readFloatLE(startWord+28, true),
-        ki: chunk.readFloatLE(startWord+32, true),
-        kd: chunk.readFloatLE(startWord+36, true),
-        ITerm: chunk.readFloatLE(startWord+40, true),
-        DTerm: chunk.readFloatLE(startWord+44, true),
-        lastInput: chunk.readFloatLE(startWord+48, true),
-        outMin: chunk.readFloatLE(startWord+52, true),
-        outMax: chunk.readFloatLE(startWord+56, true),
-        controllerDirection: chunk.readUInt8(startWord+60, true),
-        enable: chunk.readUInt8(startWord+61, true)
-    };
+    //setInterval( function() {
+    //    writer.write(command);
+    //}, 2000);
 
-    if (length > 65) {
-        output.extra = chunk.readFloatLE(startWord+62, true);
-    }
 
-    return JSON.stringify(output) + '\n';
-}
-
-sp.open(function (error) {
-    if (error) {
-        console.log('failed to open: ' + error);
-    }
-    else {
-        console.log('open');
-        sp.flush(function (err) {
-            if (err) console.log('could not flush: ', err);
-            sp
-                .pipe(through2(function(chunk, enc, callback) {
-
-                    var length = chunk.readUInt16BE(0, true);
-                    var msgType = chunk.readInt8(2, true);
-                    var startWord = 3;
-
-                    switch (msgType) {
-                        case 1:
-                            var dataMessage = parseDataMessage(chunk.slice(startWord), length);
-                            this.push(dataMessage);
-                            break;
-
-                        case 2:
-                            this.push('Message type 2: ');
-                            var dataMessage = parseDataMessage(chunk.slice(startWord), length);
-                            this.push(dataMessage);
-                            break;
-
-                        default:
-                            this.push('Bad Message Type!');
-                            break;
-                    }
-
-                    callback();
-
-                }))
-                .pipe(process.stdout);
-
-        });
-    }
 });
+
+arduinoCom._port.on('SyncFound', function() { console.log('Found Sync'); } );
+arduinoCom._port.on('disconnect', function() { console.log('Disconnected'); } );
+arduinoCom._port.on('SyncLost', function() { console.log('Lost Sync'); } );
 
